b487cdc5a9ee46427211ad24a8851d32f508552e
Jacob Nilsson <jacob@jacobnilsson.com>
2025-05-22 20:27:01 +0200

    README and example changes

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
 # ssock
+
+Simple library for sockets and HTTP in C++.
+
+## Why
+
+Well, first of all, it has zero dependencies whatsoever, so it is equivalent to using your system's exposed C headers.
+The problem with these C headers though is that... well, first of all, it's C. We use C++ around here.
+Second of all, that brings all the quirks C has with it. Third, while this doesn't as of now support anything but *BSD, Linux and macOS,
+it can trivially be extended in the future to support Windows and other operating systems with no rewrites for API users.
+
+It was also just a project made out of a desire to learn the Unix sockets API for fun. Originally I intended to use it as a base
+for a wrapper around the Nintendo Wii `network.h` API, but due to how garbage that header is, I temporarily put the project on hold,
+polished this up and released it as a standalone library for Unix.
+
+## Features
+
+- Binding, connecting, sending, receiving and closing synchronous sockets
+- (Basic) HTTP body parser, including headers and body.
+- IPv4 and IPv6 support
+- TCP and UDP support
+- Exceptions for errors
+- Inheritable classes for easy extension
+- C++23
+
+Still missing:
+
+- Server abstraction
+- Asynchronous sockets
+- SSL/TLS support (would require external dependencies)
+- Custom exceptions
+
+## Dependencies
+
+- C++23 compiler
+- CMake
+
+## Building
+
+```bash
+mkdir -p build/; cd build/
+cmake .. -DCMAKE_BUILD_TYPE=Release
+cmake --build .
+cmake --install .
+```
+
+## Usage
+
+See `examples/` for examples of how to use the library.
+
+## License
+
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for details.
+
+Copyright (c) 2025 Jacob Nilsson
---
+++ examples/socket/main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <fstream>
+#include <string_view>
+#include <ssock.hpp>
+
+/* The ssock::sock::sync_sock class is modelled to be a more modern variant of Unix
+ * sockets. It does not replace them, rather it simply wraps them.
+ * The interface is more or less retained with minimal changes. If you know how to
+ * use Unix sockets, you will know how to use this class.
+ */
+int main() {
+    /* sock_addr will automatically call resolve_hostname() for DNS resolution.
+     * The last parameter is used to determine the address type.
+     * type::hostname will resolve the hostname to an IPv6 address if available,
+     * otherwise it will resolve to an IPv4 address.
+     * If you have an IP address, use type::ipv4 or type::ipv6.
+     */
+    ssock::sock::sock_addr addr("google.com", 80, ssock::sock::sock_addr_type::hostname);
+    /* This will create a socket. It will not bind or connect on its own.
+     * You need to call connect() or bind() yourself.
+     * The socket type is tcp by default, but you can change it to udp if needed.
+     */
+    ssock::sock::sync_sock sock(addr, ssock::sock::sock_type::tcp);
+    /* This will connect to the server. It will throw an exception if it fails.
+     * You can also use bind() to bind the socket to a local address.
+     */
+    sock.connect();
+    /* Here's a simple HTTP request body I crafted for you. The socket API (obviously) doesn't implement any HTTP specifics. */
+    constexpr std::string_view request = "GET / HTTP/1.1\r\nHost: google.com\r\nConnection: close\r\n\r\n";
+    /* This will send the request to the server. It will throw an exception if it fails.
+     * You can also use send() to send data to the server.
+     */
+    sock.send(request.data());
+    /* This will receive the response from the server. It will throw an exception if it fails.
+     * You can also use recv() to receive data from the server.
+     * The timeout is set to -1, which means no timeout. You can change it to a positive value if needed.
+     */
+    std::string response = sock.recv(-1);
+    /* This will close the connection. This will be done automatically for you by the destructor, but I'm
+     * calling it manually here just to show that it can be manually invoked if needed.
+     */
+    sock.close();
+
+    /* Now simply write it to file */
+    std::ofstream file("response.txt");
+    if (file.is_open()) {
+        file << response;
+        file.close();
+    } else {
+        std::cerr << "Failed to open file" << std::endl;
+    }
+    std::cout << "Response written to response.txt" << std::endl;
+}
